5fc026b7b8f79261bd825bf40d89e21f37eea1d7
orhun <orhunparmaksiz@gmail.com>
2021-05-26 00:02:24 +0300

    feat(changelog): add capitalize_first filter
    Signed-off-by: orhun <orhunparmaksiz@gmail.com>

--- gitolith-core/src/changelog.rs
+++ gitolith-core/src/changelog.rs
@@ -1,8 +1,11 @@
 use crate::error::Result;
 use crate::release::Release;
+use std::collections::HashMap;
 use tera::{
 	Context as TeraContext,
+	Result as TeraResult,
 	Tera,
+	Value,
 };
 
 /// Changelog generator.
@@ -16,7 +19,23 @@ impl Changelog {
 	pub fn new(template: String) -> Result<Self> {
 		let mut tera = Tera::default();
 		tera.add_raw_template("changelog_template", &template)?;
+		tera.register_filter("capitalize_first", Self::capitalize_first_filter);
 		Ok(Self { tera })
+	}
+
+	/// Filter for capitalizing the first character of a string.
+	fn capitalize_first_filter(
+		value: &Value,
+		_: &HashMap<String, Value>,
+	) -> TeraResult<Value> {
+		let mut s =
+			tera::try_get_value!("capitalize_first_filter", "value", String, value);
+		let mut c = s.chars();
+		s = match c.next() {
+			None => String::new(),
+			Some(f) => f.to_uppercase().collect::<String>() + c.as_str(),
+		};
+		Ok(tera::to_value(&s).unwrap())
 	}
 
 	/// Generates the changelog.
@@ -39,7 +58,7 @@ mod test {
 		## {{ version }}
 		{% for commit in commits %}
 		### {{ commit.group }}
-		- {{ commit.message }}
+		- {{ commit.message | capitalize_first }}
 		{% endfor %}"#;
 		let changelog = Changelog::new(template.to_string())?;
 		assert_eq!(
@@ -47,10 +66,10 @@ mod test {
 		## 1.0
 		
 		### feat
-		- add xyz
+		- Add xyz
 		
 		### fix
-		- fix abc
+		- Fix abc
 		"#,
 			changelog.generate(Release {
 				version:   Some(String::from("1.0")),
