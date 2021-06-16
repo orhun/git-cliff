3cb67618ef475bf0eb144c5b84cd68af0758f564
orhun <orhunparmaksiz@gmail.com>
2021-06-16 23:07:00 +0300

    chore(release): strip the markdown format from tag message

--- release.sh
+++ release.sh
@@ -3,8 +3,9 @@
 if [ -n "$1" ]; then
 	cargo run -- --tag "$1" > CHANGELOG.md
 	git add -A && git commit -m "chore(release): prepare for $1"
+	changelog=$(cargo run -- --unreleased --strip all)
 	git -c user.name="git-cliff" \
 		-c user.email="git-cliff@protonmail.com" \
 		-c user.signingkey="1D2D410A741137EBC544826F4A92FA17B6619297" \
-		tag -s -a "$1" -m "$(cargo run -- --unreleased --strip all)"
+		tag -s -a "$1" -m "${changelog//### /}"
 fi
