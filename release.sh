6099b2d0c7c7d51a44a0e05a53908a9a370a7e83
orhun <orhunparmaksiz@gmail.com>
2021-06-16 23:14:27 +0300

    chore(release): add release title to the tag message

--- release.sh
+++ release.sh
@@ -7,5 +7,5 @@ if [ -n "$1" ]; then
 	git -c user.name="git-cliff" \
 		-c user.email="git-cliff@protonmail.com" \
 		-c user.signingkey="1D2D410A741137EBC544826F4A92FA17B6619297" \
-		tag -s -a "$1" -m "${changelog//### /}"
+		tag -s -a "$1" -m "Release $1\n\n${changelog//### /}"
 fi
