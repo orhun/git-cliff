c7f08fe4a3113f656bb45a29141aa127d4e453e5
orhun <orhunparmaksiz@gmail.com>
2021-06-19 23:51:44 +0300

    fix(release): strip the unreleased title from tag message

--- release.sh
+++ release.sh
@@ -4,8 +4,10 @@ if [ -n "$1" ]; then
 	cargo run -- --tag "$1" > CHANGELOG.md
 	git add -A && git commit -m "chore(release): prepare for $1"
 	changelog=$(cargo run -- --unreleased --strip all)
+	changelog=${changelog//### /}
+	changelog=${changelog//## [unreleased\]/}
 	git -c user.name="git-cliff" \
 		-c user.email="git-cliff@protonmail.com" \
 		-c user.signingkey="1D2D410A741137EBC544826F4A92FA17B6619297" \
-		tag -s -a "$1" -m "Release $1\n\n${changelog//### /}"
+		tag -s -a "$1" -m "Release $1" -m "$changelog"
 fi
