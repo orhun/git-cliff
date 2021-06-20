381c941a94188fb40bcce1129c29e6a5379ac7a7
orhun <orhunparmaksiz@gmail.com>
2021-06-20 21:33:22 +0300

    fix(release): specify the committer email in release script

--- release.sh
+++ release.sh
@@ -15,7 +15,9 @@ if [ -n "$1" ]; then
 	{% endfor %}"
 	changelog=$(cargo run -- --unreleased --strip all)
 	# create a tag
-	git -c user.signingkey="1D2D410A741137EBC544826F4A92FA17B6619297" \
+	git -c user.name="git-cliff" \
+		-c user.email="git-cliff@protonmail.com" \
+		-c user.signingkey="1D2D410A741137EBC544826F4A92FA17B6619297" \
 		tag -s -a "$1" -m "Release $1" -m "$changelog"
 else
 	echo "warn: please provide a tag"
