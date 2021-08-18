45889c0a05ad64598d4e6b053bc0d90ff5449b51
orhun <orhunparmaksiz@gmail.com>
2021-08-18 03:59:08 +0300

    fix(config): use custom error type for UTF-8 errors

--- git-cliff-core/src/error.rs
+++ git-cliff-core/src/error.rs
@@ -6,6 +6,10 @@ pub enum Error {
 	/// Error that may occur while I/O operations.
 	#[error("IO error: `{0}`")]
 	IoError(#[from] std::io::Error),
+	/// Error that may occur when attempting to interpret a sequence of u8 as a
+	/// string.
+	#[error("UTF-8 error: `{0}`")]
+	Utf8Error(#[from] std::str::Utf8Error),
 	/// Error variant that represents errors coming out of libgit2.
 	#[error("Git error: `{0}`")]
 	GitError(#[from] git2::Error),
