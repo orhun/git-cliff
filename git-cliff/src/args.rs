c62ca036521d132842e8baada3d0c2b76d36c999
Taylan Dogan <git@taylandogan.info>
2021-09-28 17:34:35 +0300

    refactor: add sorting flag
    I don't expect any other sorting types will be added
    so the logic consist of checking whether it is
    `newest` or not.
    
    One could argue with why wouldn't I make this a boolean.
    My answer would be, in my opinion, it lose its meaning
    because this is not something we want to enable or disable
    but something that we want to decide which pattern
    we want to use. So it is more like a semantic choice.
    
    Signed-off-by: Taylan Dogan <git@taylandogan.info>

--- git-cliff/src/args.rs
+++ git-cliff/src/args.rs
@@ -77,4 +77,20 @@ pub struct Opt {
 	/// Sets the commit range to process.
 	#[structopt(value_name = "RANGE")]
 	pub range:       Option<String>,
+
+	/// Sets sorting of the commits inside the sections
+	///
+	/// If sort is `oldest`, the oldest commit will be on top. {n}
+	/// - PR #1 {n}
+	/// - PR #2
+	///
+	/// If sort is `newest`, the newest commit will be on top. {n}
+	/// - PR #2 {n}
+	/// - PR #1 {n}
+	#[structopt(
+		long,
+		possible_values = &["oldest", "newest"],
+		default_value = "oldest"
+	)]
+	pub sort: String,
 }
