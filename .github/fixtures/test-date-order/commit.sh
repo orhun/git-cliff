443525cb0289cb046a28e6c73136417be159138e
Kenji Miyake <kenji.miyake@tier4.jp>
2022-02-06 05:44:55 +0900

    set GIT_COMMITTER_DATE for each commit
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

--- .github/fixtures/test-date-order/commit.sh
+++ .github/fixtures/test-date-order/commit.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-git commit --allow-empty -m "feat: add feature 1"
+GIT_COMMITTER_DATE="2021-01-23 01:23:45" git commit --allow-empty -m "feat: add feature 1"
 git tag v0.1.0
-sleep 1
 
-git commit --allow-empty -m "feat: add feature 2"
+GIT_COMMITTER_DATE="2021-01-23 01:23:46" git commit --allow-empty -m "feat: add feature 2"
 git tag v0.2.0
-sleep 1
 
 git checkout v0.1.0
-git commit --allow-empty -m "feat: fix feature 1"
+GIT_COMMITTER_DATE="2021-01-23 01:23:47" git commit --allow-empty -m "feat: fix feature 1"
 git tag v0.1.1
-sleep 1
