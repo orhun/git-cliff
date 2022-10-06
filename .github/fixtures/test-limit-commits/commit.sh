7d7f3d05a4c50d7e0cdc6d3f0de19a470ec1bc7c
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2022-10-06 13:41:14 +0300

    test(fixture): add test fixture for limiting commits

---
+++ .github/fixtures/test-limit-commits/commit.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+
+GIT_COMMITTER_DATE="2022-04-06 01:25:08" git commit --allow-empty -m "Initial commit"
+GIT_COMMITTER_DATE="2022-04-06 01:25:09" git commit --allow-empty -m "feat: add feature 1"
+GIT_COMMITTER_DATE="2022-04-06 01:25:10" git commit --allow-empty -m "fix: fix feature 1 (#1)"
+GIT_COMMITTER_DATE="2022-04-06 01:25:11" git commit --allow-empty -m "refactor: move  classes"
+git tag v0.1.0
