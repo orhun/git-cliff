d415ff5f4b6fb716d3041e158c9d86a3f786800c
Burak Güner <burak.guner@callstack.com>
2023-01-06 16:47:07 +0300

    feat: execute git cliff binary using nodejs

--- packaging/npm/git-cliff/src/index.ts
+++ packaging/npm/git-cliff/src/index.ts
@@ -1,3 +1,38 @@
 #!/usr/bin/env node
 
-console.log("test")
+import { spawnSync } from "child_process"
+
+/**
+  * Returns the executable path for git-cliff located inside node_modules
+  * The naming convention is git-cliff-${os}-${arch}
+  * If the platform is `win32` or `cygwin`, executable will include a `.exe` extension
+  * @see https://nodejs.org/api/os.html#osarch
+  * @see https://nodejs.org/api/os.html#osplatform
+  * @example "x/xx/node_modules/git-cliff-darwin-arm64"
+  */
+function getExePath() {
+  const arch = process.arch;
+  const os = process.platform;
+
+  let extension = '';
+  if (['win32', 'cygwin'].includes(process.platform)) {
+    extension = '.exe';
+  }
+
+  try {
+    // Since the bin will be located inside `node_modules`, we can simply call require.resolve
+    return require.resolve(`git-cliff-${os}-${arch}/bin/git-cliff${extension}`)
+  } catch (e) {
+    throw new Error(`Couldn't find git-cliff binary inside node_modules for ${os}-${arch}`)
+  }
+}
+
+/**
+  * Runs `git-cliff` with args using nodejs spawn
+  */
+function runGitCliff() {
+  const args = process.argv.slice(2)
+  spawnSync(getExePath(), args, { stdio: "inherit" })
+}
+
+runGitCliff()
