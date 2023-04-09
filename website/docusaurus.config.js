5644d1036508ffb420e12503adb671708e087cd9
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2023-04-09 18:16:31 +0300

    chore(website): move website to website folder
    This reverts commit b6e52e128e7a105682adf885850d14deefdff3ec.

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -41,13 +41,13 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/orhun/git-cliff/tree/main/docs/",
+          editUrl: "https://github.com/orhun/git-cliff/tree/main/website/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/orhun/git-cliff/tree/main/docs/",
+          editUrl: "https://github.com/orhun/git-cliff/tree/main/website/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
