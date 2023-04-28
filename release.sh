b3eef4a6889cdedac1c0760455849067ef2d887d
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2023-04-28 15:11:11 +0300

    fix(release): use the correct argument in release script

--- release.sh
+++ release.sh
@@ -13,7 +13,7 @@ sed -E -i "s/^version = .* $msg$/version = \"${1#v}\" $msg/" git-cliff*/Cargo.to
 sed -E -i "s/\"version\": \".+\"/\"version\": \"${1#v}\"/" npm/git-cliff/package.json
 sed -E -i "s/\"(git-cliff-.+)\": \".+\"/\"\1\": \"${1#v}\"/g" npm/git-cliff/package.json
 # update the changelog
-cargo run -- --config cliff.toml -tag "$1" >CHANGELOG.md
+cargo run -- --config cliff.toml --tag "$1" >CHANGELOG.md
 git add -A && git commit -m "chore(release): prepare for $1"
 git show
 # generate a changelog for the tag message
