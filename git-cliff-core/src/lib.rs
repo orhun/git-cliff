db9c7a47decf80dc53ae613671ce67ee2b66e481
Marco Ieni <11428655+MarcoIeni@users.noreply.github.com>
2023-04-30 17:47:56 +0200

    add doc

--- git-cliff-core/src/lib.rs
+++ git-cliff-core/src/lib.rs
@@ -1,4 +1,9 @@
 //! A highly customizable changelog generator
+//!
+//! ## Features
+//! - `repo`: Enable parsing commits from a git repository. Enabled by default.
+//!   You can turn this off if you already have the commits to put in the
+//!   changelog and you don't need `git-cliff` to parse them.
 #![warn(missing_docs, clippy::unwrap_used)]
 
 /// Changelog generator.
