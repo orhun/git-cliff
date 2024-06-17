9479ef1aafe26d74b7a7e103265e05cf64937575
Bukowa <gitbukowa@gmail.com>
2024-06-17 17:30:53 +0200

    tests(fixtures): uncomment commit

--- .github/fixtures/test-cli-arg-ignore-tags/commit.sh
+++ .github/fixtures/test-cli-arg-ignore-tags/commit.sh
@@ -8,7 +8,7 @@ GIT_COMMITTER_DATE="2021-01-23 01:23:46" git commit --allow-empty -m "feat: add 
 git tag v1.0.0-beta.2
 
 # WARNING: If we won't create this commit, 1.0.0 won't be created!
-#GIT_COMMITTER_DATE="2021-01-23 01:23:47" git commit --allow-empty -m "chore: why do i need a commit here?"
+GIT_COMMITTER_DATE="2021-01-23 01:23:47" git commit --allow-empty -m "chore: why do i need a commit here?"
 git tag v1.0.0
 
 GIT_COMMITTER_DATE="2021-01-23 01:23:49" git commit --allow-empty -m "fix: simple fix"
