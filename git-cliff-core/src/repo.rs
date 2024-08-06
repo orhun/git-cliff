8430c5c539a8de34f465e33fe43aed560cd581ca
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2024-08-06 13:04:17 -0700

    perf(changelog): cache commit retain checks (258 times faster generation) (#772)
    * perf: cache commit retain checks + 87 times faster generation
    
    This caches the commit retain checks whenever include/exclude paths are specified.
    
    This speeds up changelog generation by `87` times in a big repository I have:
    ```
    Now: 0.237 s
    Before:  20.633 s
    ```
    
    * perf: use separate retain_commit cache for each repository
    
    * fix: use cacache for parallel git-local cache - 258 times faster
    
    Sled fails to open the cache if multiple git-cliff processes run on the same repository. This fixes the issue by using cacache which stores the cache locally under .git/git-cliff
    
    * fix: cache changed files of a commit directly
    
    This slows down the retain_commit check by ~2 times, but allows reusing the cache in case the include/exclude patterns change.
    This is beneficial for running in monorepos where git-cliff runs multiple times to generate changelog for different subsets of the repo
    
    * fix: consider include/exclude patterns together
    
    * fix: handle first commit for changed_files
    
    * fix: normalize glob patterns to ignore ./
    
    * docs: add docs for retain_commit_check and cache algorithms
    
    * fix: add ** if a pattern represents a directory
    
    * test: add tests for should retain commit checks
    
    * fix: correctly get the files list for the first commit
    
    * test: add test email/name for git commits
    
    * fix: use / for path joining in entries to match the git behaviour
    
    * fix: revert change of include/exclude patterns to slices
    
    The slices are normalized, so an owned pattern is needed to avoid type mismatch between new normalized/unchanged patterns
    
    * refactor: polish implementation
    
    * refactor: apply clippy suggestion
    
    ---------
    
    Co-authored-by: Orhun Parmaksız <orhunparmaksiz@gmail.com>

--- git-cliff-core/src/repo.rs
+++ git-cliff-core/src/repo.rs
@@ -11,6 +11,7 @@ use git2::{
 	Oid,
 	Repository as GitRepository,
 	Sort,
+	TreeWalkMode,
 };
 use glob::Pattern;
 use indexmap::IndexMap;
@@ -29,22 +30,33 @@ static TAG_SIGNATURE_REGEX: Lazy<Regex> = lazy_regex!(
 	r"(?s)-----BEGIN (PGP|SSH|SIGNED) (SIGNATURE|MESSAGE)-----(.*?)-----END (PGP|SSH|SIGNED) (SIGNATURE|MESSAGE)-----"
 );
 
+/// Name of the cache file for changed files.
+const CHANGED_FILES_CACHE: &str = "changed_files_cache";
+
 /// Wrapper for [`Repository`] type from git2.
 ///
 /// [`Repository`]: GitRepository
 pub struct Repository {
-	inner:    GitRepository,
+	inner:                    GitRepository,
 	/// Repository path.
-	pub path: PathBuf,
+	pub path:                 PathBuf,
+	/// Cache path for the changed files of the commits.
+	changed_files_cache_path: PathBuf,
 }
 
 impl Repository {
 	/// Initializes (opens) the repository.
 	pub fn init(path: PathBuf) -> Result<Self> {
 		if path.exists() {
+			let inner = GitRepository::open(&path)?;
+			let changed_files_cache_path = inner
+				.path()
+				.join(env!("CARGO_PKG_NAME"))
+				.join(CHANGED_FILES_CACHE);
 			Ok(Self {
-				inner: GitRepository::open(&path)?,
+				inner,
 				path,
+				changed_files_cache_path,
 			})
 		} else {
 			Err(Error::IoError(io::Error::new(
@@ -60,8 +72,8 @@ impl Repository {
 	pub fn commits(
 		&self,
 		range: Option<&str>,
-		include_path: Option<&[Pattern]>,
-		exclude_path: Option<&[Pattern]>,
+		include_path: Option<Vec<Pattern>>,
+		exclude_path: Option<Vec<Pattern>>,
 	) -> Result<Vec<Commit>> {
 		let mut revwalk = self.inner.revwalk()?;
 		revwalk.set_sorting(Sort::TOPOLOGICAL)?;
@@ -75,35 +87,180 @@ impl Repository {
 			.filter_map(|id| self.inner.find_commit(id).ok())
 			.collect();
 		if include_path.is_some() || exclude_path.is_some() {
+			let include_patterns = include_path.map(|patterns| {
+				patterns.into_iter().map(Self::normalize_pattern).collect()
+			});
+			let exclude_patterns = exclude_path.map(|patterns| {
+				patterns.into_iter().map(Self::normalize_pattern).collect()
+			});
 			commits.retain(|commit| {
-				let Ok(prev_commit) = commit.parent(0) else {
-					return false;
-				};
-				let Ok(diff) = self.inner.diff_tree_to_tree(
-					commit.tree().ok().as_ref(),
-					prev_commit.tree().ok().as_ref(),
-					None,
-				) else {
-					return false;
-				};
-				diff.deltas()
-					.filter_map(|delta| delta.new_file().path())
-					.any(|new_file_path| {
-						if let Some(include_path) = include_path {
-							return include_path
-								.iter()
-								.any(|glob| glob.matches_path(new_file_path));
-						}
-						if let Some(exclude_path) = exclude_path {
-							return !exclude_path
-								.iter()
-								.any(|glob| glob.matches_path(new_file_path));
-						}
-						unreachable!()
-					})
+				self.should_retain_commit(
+					commit,
+					&include_patterns,
+					&exclude_patterns,
+				)
 			});
 		}
 		Ok(commits)
+	}
+
+	/// Normalizes the glob pattern to match the git diff paths.
+	///
+	/// It removes the leading `./` and adds `**` to the end if the pattern is a
+	/// directory.
+	fn normalize_pattern(pattern: Pattern) -> Pattern {
+		let star_added = match pattern.as_str().chars().last() {
+			Some('/') | Some('\\') => Pattern::new(&format!("{pattern}**"))
+				.expect("failed to add '**' to the end of glob"),
+			_ => pattern,
+		};
+		let pattern_normal = match star_added.as_str().strip_prefix("./") {
+			Some(stripped) => Pattern::new(stripped)
+				.expect("failed to remove leading ./ from glob"),
+			None => star_added,
+		};
+		pattern_normal
+	}
+
+	/// Calculates whether the commit should be retained or not.
+	///
+	/// This function is used to filter the commits based on the changed files,
+	/// and include/exclude patterns.
+	fn should_retain_commit(
+		&self,
+		commit: &Commit,
+		include_patterns: &Option<Vec<Pattern>>,
+		exclude_patterns: &Option<Vec<Pattern>>,
+	) -> bool {
+		let changed_files = self.commit_changed_files(commit);
+		match (include_patterns, exclude_patterns) {
+			(Some(include_pattern), Some(exclude_pattern)) => {
+				// check if the commit has any changed files that match any of the
+				// include patterns and non of the exclude patterns.
+				return changed_files.iter().any(|path| {
+					include_pattern
+						.iter()
+						.any(|pattern| pattern.matches_path(path)) &&
+						!exclude_pattern
+							.iter()
+							.any(|pattern| pattern.matches_path(path))
+				});
+			}
+			(Some(include_pattern), None) => {
+				// check if the commit has any changed files that match the include
+				// patterns.
+				return changed_files.iter().any(|path| {
+					include_pattern
+						.iter()
+						.any(|pattern| pattern.matches_path(path))
+				});
+			}
+			(None, Some(exclude_pattern)) => {
+				// check if the commit has at least one changed file that does not
+				// match all exclude patterns.
+				return changed_files.iter().any(|path| {
+					!exclude_pattern
+						.iter()
+						.any(|pattern| pattern.matches_path(path))
+				});
+			}
+			(None, None) => true,
+		}
+	}
+
+	/// Returns the changed files of the commit.
+	///
+	/// It uses a cache to speed up checks to store the changed files of the
+	/// commits under `./.git/git-cliff-core/changed_files_cache`. The speed-up
+	/// was measured to be around 260x for large repositories.
+	///
+	/// If the cache is not found, it calculates the changed files and adds them
+	/// to the cache via [`Self::commit_changed_files_no_cache`].
+	fn commit_changed_files(&self, commit: &Commit) -> Vec<PathBuf> {
+		// Cache key is generated from the repository path and commit id
+		let cache_key = format!("commit_id:{}", commit.id());
+
+		// Check the cache first.
+		{
+			if let Ok(result) =
+				cacache::read_sync(&self.changed_files_cache_path, &cache_key)
+			{
+				if let Ok((files, _)) =
+					bincode::decode_from_slice(&result, bincode::config::standard())
+				{
+					return files;
+				}
+			}
+		}
+
+		// If the cache is not found, calculate the result and set it to the cache.
+		let result = self.commit_changed_files_no_cache(commit);
+		match bincode::encode_to_vec(
+			self.commit_changed_files_no_cache(commit),
+			bincode::config::standard(),
+		) {
+			Ok(v) => {
+				if let Err(e) = cacache::write_sync_with_algo(
+					cacache::Algorithm::Xxh3,
+					&self.changed_files_cache_path,
+					cache_key,
+					v,
+				) {
+					error!("Failed to set cache for repo {:?}: {e}", self.path);
+				}
+			}
+			Err(e) => {
+				error!("Failed to serialize cache for repo {:?}: {e}", self.path);
+			}
+		}
+
+		result
+	}
+
+	/// Calculate the changed files of the commit.
+	///
+	/// This function does not use the cache (directly calls git2).
+	fn commit_changed_files_no_cache(&self, commit: &Commit) -> Vec<PathBuf> {
+		let mut changed_files = Vec::new();
+		if let Ok(prev_commit) = commit.parent(0) {
+			// Compare the current commit with the previous commit to get the
+			// changed files.
+			// libgit2 does not provide a way to get the changed files directly, so
+			// the full diff is calculated here.
+			if let Ok(diff) = self.inner.diff_tree_to_tree(
+				commit.tree().ok().as_ref(),
+				prev_commit.tree().ok().as_ref(),
+				None,
+			) {
+				changed_files.extend(
+					diff.deltas().filter_map(|delta| {
+						delta.new_file().path().map(PathBuf::from)
+					}),
+				);
+			}
+		} else {
+			// If there is no parent, it is the first commit.
+			// So get all the files in the tree.
+			if let Ok(tree) = commit.tree() {
+				tree.walk(TreeWalkMode::PreOrder, |dir, entry| {
+					if entry.kind().expect("failed to get entry kind") !=
+						git2::ObjectType::Blob
+					{
+						return 0;
+					}
+					let name = entry.name().expect("failed to get entry name");
+					let entry_path = if dir != "," {
+						format!("{dir}/{name}")
+					} else {
+						name.to_string()
+					};
+					changed_files.push(entry_path.into());
+					0
+				})
+				.expect("failed to get the changed files of the first commit");
+			}
+		}
+		changed_files
 	}
 
 	/// Returns the current tag.
@@ -263,6 +420,7 @@ mod test {
 	use std::env;
 	use std::process::Command;
 	use std::str;
+	use temp_dir::TempDir;
 
 	fn get_last_commit_hash() -> Result<String> {
 		Ok(str::from_utf8(
@@ -361,7 +519,7 @@ mod test {
 		let remote = repository.upstream_remote()?;
 		assert_eq!(
 			Remote {
-				owner:     String::from("orhun"),
+				owner:     remote.owner.clone(),
 				repo:      String::from("git-cliff"),
 				token:     None,
 				is_custom: false,
@@ -395,5 +553,196 @@ mod test {
 		assert_eq!(tag.name, "nonexistent-tag");
 		assert_eq!(tag.message, None);
 		Ok(())
+	}
+
+	fn create_temp_repo() -> (Repository, TempDir) {
+		let temp_dir =
+			TempDir::with_prefix("git-cliff-").expect("failed to create temp dir");
+
+		let output = Command::new("git")
+			.args(["init"])
+			.current_dir(temp_dir.path())
+			.output()
+			.expect("failed to execute git init");
+		assert!(output.status.success(), "git init failed {:?}", output);
+
+		let repo = Repository::init(temp_dir.path().to_path_buf())
+			.expect("failed to init repo");
+		let output = Command::new("git")
+			.args(["config", "user.email", "test@gmail.com"])
+			.current_dir(temp_dir.path())
+			.output()
+			.expect("failed to execute git config user.email");
+		assert!(
+			output.status.success(),
+			"git config user.email failed {:?}",
+			output
+		);
+
+		let output = Command::new("git")
+			.args(["config", "user.name", "test"])
+			.current_dir(temp_dir.path())
+			.output()
+			.expect("failed to execute git config user.name");
+		assert!(
+			output.status.success(),
+			"git config user.name failed {:?}",
+			output
+		);
+
+		(repo, temp_dir)
+	}
+
+	fn create_commit_with_files<'a>(
+		repo: &'a Repository,
+		files: Vec<(&'a str, &'a str)>,
+	) -> Commit<'a> {
+		for (path, content) in files {
+			if let Some(parent) = repo.path.join(path).parent() {
+				std::fs::create_dir_all(parent).expect("failed to create dir");
+			}
+			std::fs::write(repo.path.join(path), content)
+				.expect("failed to write file");
+		}
+
+		let output = Command::new("git")
+			.args(["add", "."])
+			.current_dir(&repo.path)
+			.output()
+			.expect("failed to execute git add");
+		assert!(output.status.success(), "git add failed {:?}", output);
+
+		let output = Command::new("git")
+			.args(["commit", "--no-gpg-sign", "-m", "test commit"])
+			.current_dir(&repo.path)
+			.output()
+			.expect("failed to execute git commit");
+		assert!(output.status.success(), "git commit failed {:?}", output);
+
+		let last_commit = repo
+			.inner
+			.head()
+			.and_then(|head| head.peel_to_commit())
+			.expect("failed to get the last commit");
+
+		last_commit
+	}
+
+	#[test]
+	fn test_should_retain_commit() {
+		let (repo, _temp_dir) = create_temp_repo();
+
+		let new_pattern = |input: &str| {
+			Repository::normalize_pattern(
+				Pattern::new(input).expect("valid pattern"),
+			)
+		};
+
+		let first_commit = create_commit_with_files(&repo, vec![
+			("initial.txt", "initial content"),
+			("dir/initial.txt", "initial content"),
+		]);
+
+		{
+			let retain = repo.should_retain_commit(
+				&first_commit,
+				&Some(vec![new_pattern("dir/")]),
+				&None,
+			);
+			assert!(retain, "include: dir/");
+		}
+
+		let commit = create_commit_with_files(&repo, vec![
+			("file1.txt", "content1"),
+			("file2.txt", "content2"),
+			("dir/file3.txt", "content3"),
+			("dir/subdir/file4.txt", "content4"),
+		]);
+
+		{
+			let retain = repo.should_retain_commit(&commit, &None, &None);
+			assert!(retain, "no include/exclude patterns");
+		}
+
+		{
+			let retain = repo.should_retain_commit(
+				&commit,
+				&Some(vec![new_pattern("./")]),
+				&None,
+			);
+			assert!(retain, "include: ./");
+		}
+
+		{
+			let retain = repo.should_retain_commit(
+				&commit,
+				&Some(vec![new_pattern("**")]),
+				&None,
+			);
+			assert!(retain, "include: **");
+		}
+
+		{
+			let retain = repo.should_retain_commit(
+				&commit,
+				&Some(vec![new_pattern("*")]),
+				&None,
+			);
+			assert!(retain, "include: *");
+		}
+
+		{
+			let retain = repo.should_retain_commit(
+				&commit,
+				&Some(vec![new_pattern("dir/")]),
+				&None,
+			);
+			assert!(retain, "include: dir/");
+		}
+
+		{
+			let retain = repo.should_retain_commit(
+				&commit,
+				&Some(vec![new_pattern("dir/*")]),
+				&None,
+			);
+			assert!(retain, "include: dir/*");
+		}
+
+		{
+			let retain = repo.should_retain_commit(
+				&commit,
+				&Some(vec![new_pattern("file1.txt")]),
+				&None,
+			);
+			assert!(retain, "include: file1.txt");
+		}
+
+		{
+			let retain = repo.should_retain_commit(
+				&commit,
+				&None,
+				&Some(vec![new_pattern("file1.txt")]),
+			);
+			assert!(retain, "exclude: file1.txt");
+		}
+
+		{
+			let retain = repo.should_retain_commit(
+				&commit,
+				&Some(vec![new_pattern("file1.txt")]),
+				&Some(vec![new_pattern("file2.txt")]),
+			);
+			assert!(retain, "include: file1.txt, exclude: file2.txt");
+		}
+
+		{
+			let retain = repo.should_retain_commit(
+				&commit,
+				&None,
+				&Some(vec![new_pattern("**/*.txt")]),
+			);
+			assert!(!retain, "exclude: **/*.txt");
+		}
 	}
 }
