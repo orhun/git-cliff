a607a25c936a9d87042694d35f2265c6746dbbcc
Binbin Ye <phantom2501@gmail.com>
2024-08-24 14:28:06 +0900

    fix typo

--- git-cliff-core/src/repo.rs
+++ git-cliff-core/src/repo.rs
@@ -313,7 +313,7 @@ impl Repository {
 	/// Decide whether to include tag
 	///
 	/// `head_commit` is the `latest` commit to generate changelog. It can be a
-	/// branch head or a detached head. `commit` is a tagged commmit. If the
+	/// branch head or a detached head. `commit` is a tagged commit. If the
 	/// commit is in the descendant graph of the head_commit or is the
 	/// head_commit itself, Changelog should include the tag.
 	fn should_include_tag(
