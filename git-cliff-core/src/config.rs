002ae3c5de91f10ae91a51f3a0e942856aae7404
Nejc Galof <galof.nejc@gmail.com>
2024-09-23 20:30:43 +0200

    refactor(clippy): apply single_match_else lint (#878)

--- git-cliff-core/src/config.rs
+++ git-cliff-core/src/config.rs
@@ -260,21 +260,17 @@ impl Bump {
 	///
 	/// This function also logs the returned value.
 	pub fn get_initial_tag(&self) -> String {
-		match self.initial_tag.clone() {
-			Some(tag) => {
-				warn!(
-					"No releases found, using initial tag '{tag}' as the next \
-					 version."
-				);
-				tag
-			}
-			None => {
-				warn!(
-					"No releases found, using {DEFAULT_INITIAL_TAG} as the next \
-					 version."
-				);
-				DEFAULT_INITIAL_TAG.into()
-			}
+		if let Some(tag) = self.initial_tag.clone() {
+			warn!(
+				"No releases found, using initial tag '{tag}' as the next version."
+			);
+			tag
+		} else {
+			warn!(
+				"No releases found, using {DEFAULT_INITIAL_TAG} as the next \
+				 version."
+			);
+			DEFAULT_INITIAL_TAG.into()
 		}
 	}
 }
