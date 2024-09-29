bfe9beb093df42badf762945cf04548fbe75949a
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2024-09-29 21:18:02 +0300

    docs(website): add more testimonials

--- website/src/components/Testimonials/index.tsx
+++ website/src/components/Testimonials/index.tsx
@@ -10,6 +10,30 @@ const testimonials = [
       "git-cliff is amazing. I managed to use its published crates for my project's release management.",
     url: "https://x.com/boshen_c/status/1840391571495420362",
   },
+  {
+    name: "Greptime Database",
+    handle: "GreptimeTeam",
+    date: "Apr 9, 2024",
+    content:
+      "The changelog is powered by git-cliff. Thank you @orhundev for this elegant tool!",
+    url: "https://x.com/Greptime/status/1777707539255189544",
+  },
+  {
+    name: "CodeSee.io",
+    handle: "Codesee-io",
+    date: "Jan 24, 2023",
+    content:
+      "With git-cliff, you can generate a changelog from your Git history FAST.",
+    url: "https://x.com/Codeseeio/status/1617901319179702274",
+  },
+  {
+    name: "GitHub, Inc.",
+    handle: "github",
+    date: "May 4, 2022",
+    content:
+      "Whether you use conventional commits or your own conventions, @orhundev’s git-cliff is a great little tool for generating changelogs.",
+    url: "https://x.com/github/status/1521943000817057795",
+  },
 ];
 
 const getRandomTestimonial = () => {
