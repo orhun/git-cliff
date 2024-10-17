8462d82d3472a3df68b6ea108b63c485677863f1
Albrecht <archbelt@posteo.de>
2024-10-17 15:25:42 +0200

    feat: add changelog modifier callback to run function

--- git-cliff/src/lib.rs
+++ git-cliff/src/lib.rs
@@ -347,7 +347,14 @@ fn process_repository<'a>(
 }
 
 /// Runs `git-cliff`.
-pub fn run(mut args: Opt) -> Result<()> {
+pub fn run(args: Opt) -> Result<()> {
+	run_with_changelog_modifier(args, |_| Ok(()))
+}
+
+pub fn run_with_changelog_modifier(
+	mut args: Opt,
+	changelog_modifier: impl FnOnce(&mut Changelog) -> Result<()>,
+) -> Result<()> {
 	// Check if there is a new version available.
 	#[cfg(feature = "update-informer")]
 	check_new_version();
@@ -590,6 +597,7 @@ pub fn run(mut args: Opt) -> Result<()> {
 		}
 		Changelog::new(releases, &config)?
 	};
+	changelog_modifier(&mut changelog)?;
 
 	// Print the result.
 	let mut out: Box<dyn io::Write> = if let Some(path) = &output {
