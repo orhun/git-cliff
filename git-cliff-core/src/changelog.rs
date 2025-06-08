f8b20f310167d3ed914b29f34b24f41168dc809d
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2025-06-08 11:48:55 +0900

    refactor: commit_duration_days -> commit_timespan
    Signed-off-by: Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>

--- git-cliff-core/src/changelog.rs
+++ git-cliff-core/src/changelog.rs
@@ -729,8 +729,8 @@ mod test {
 				{% endfor %}{% endfor %}
 				### Commit Statistics
 				- {{ statistics.commit_count }} commits contributed to the release.
-				{%- if statistics.commit_duration_days is defined %}
-				- {{ statistics.commit_duration_days }} days spanned between the first and last commit.
+				{%- if statistics.commit_timespan is defined %}
+				- {{ statistics.commit_timespan }} days spanned between the first and last commit.
 				{%- endif %}
 				- {{ statistics.conventional_commit_count }} commit was understood as conventional.
 				- {{ statistics.total_link_count }} issues like '(#ID)' were seen in commit messages.
