706551f8b0708f29a7e94130eafcf848edba7b90
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2025-10-22 17:28:17 +0300

    fix(fixture): fix the fixture
    fix fix ifx

--- .github/fixtures/test-workdir-include-path/commit.sh
+++ .github/fixtures/test-workdir-include-path/commit.sh
@@ -3,5 +3,6 @@ set -e
 
 git remote add origin https://github.com/orhun/git-cliff
 git fetch
+mv cliff.toml cliff.toml.bak
 git checkout 076feb74b4d8c8634669f57d4e2765c39490d80e
-mv cliff.toml .github/fixtures/
+mv cliff.toml.bak .github/fixtures/cliff.toml
