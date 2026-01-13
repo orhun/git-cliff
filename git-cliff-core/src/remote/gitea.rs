ce107b4e5af9d1824b9e5831013d9f875ea26ef9
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2026-01-13 20:26:36 +0900

    feat: add spans to remote module
    Signed-off-by: Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>

--- git-cliff-core/src/remote/gitea.rs
+++ git-cliff-core/src/remote/gitea.rs
@@ -7,12 +7,6 @@ use super::{Debug, MAX_PAGE_SIZE, RemoteClient, RemoteCommit, RemotePullRequest}
 use crate::config::Remote;
 use crate::error::{Error, Result};
 
-/// Log message to show while fetching data from Gitea.
-pub const START_FETCHING_MSG: &str = "Retrieving data from Gitea...";
-
-/// Log message to show when done fetching from Gitea.
-pub const FINISHED_FETCHING_MSG: &str = "Done fetching Gitea data.";
-
 /// Template variables related to this remote.
 pub(crate) const TEMPLATE_VARIABLES: &[&str] = &["gitea", "commit.gitea", "commit.remote"];
 
@@ -122,6 +116,7 @@ impl RemoteClient for GiteaClient {
 
 impl GiteaClient {
     /// Constructs the URL for Gitea commits API.
+    #[cfg_attr(feature = "tracing", tracing::instrument(skip_all))]
     fn commits_url(api_url: &str, remote: &Remote, ref_name: Option<&str>, page: i32) -> String {
         let mut url = format!(
             "{}/api/v1/repos/{}/{}/commits?limit={MAX_PAGE_SIZE}&page={page}",
@@ -136,6 +131,7 @@ impl GiteaClient {
     }
 
     /// Constructs the URL for Gitea pull requests API.
+    #[cfg_attr(feature = "tracing", tracing::instrument(skip_all))]
     fn pull_requests_url(api_url: &str, remote: &Remote, page: i32) -> String {
         format!(
             "{}/api/v1/repos/{}/{}/pulls?limit={MAX_PAGE_SIZE}&page={page}&state=closed",
@@ -146,6 +142,7 @@ impl GiteaClient {
     /// Fetches the complete list of commits.
     /// This is inefficient for large repositories; consider using
     /// `get_commit_stream` instead.
+    #[cfg_attr(feature = "tracing", tracing::instrument(skip_all))]
     pub async fn get_commits(&self, ref_name: Option<&str>) -> Result<Vec<Box<dyn RemoteCommit>>> {
         use futures::TryStreamExt;
         self.get_commit_stream(ref_name).try_collect().await
@@ -154,6 +151,7 @@ impl GiteaClient {
     /// Fetches the complete list of pull requests.
     /// This is inefficient for large repositories; consider using
     /// `get_pull_request_stream` instead.
+    #[cfg_attr(feature = "tracing", tracing::instrument(skip_all))]
     pub async fn get_pull_requests(&self) -> Result<Vec<Box<dyn RemotePullRequest>>> {
         use futures::TryStreamExt;
         self.get_pull_request_stream().try_collect().await
