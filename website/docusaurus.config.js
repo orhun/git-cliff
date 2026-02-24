73833c034043be065b39c03b792ffc6b5d9a1667
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2026-02-25 04:37:06 +0900

    revert: restore siteConfig.onBrokenMarkdownLinks usage
    Signed-off-by: Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -22,11 +22,7 @@ const config = {
   projectName: "git-cliff", // Usually your repo name.
 
   onBrokenLinks: "throw",
-  markdown: {
-    hooks: {
-      onBrokenMarkdownLinks: 'warn',
-    },
-  },
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
