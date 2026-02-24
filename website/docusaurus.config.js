39b67ab5a48651b29a2fd5321119f12f128155c3
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2026-02-25 03:49:23 +0900

    fix: remove explicit PluginOptions type to resolve type error
    Signed-off-by: Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -39,7 +39,6 @@ const config = {
   themes: [
     [
       require.resolve("@easyops-cn/docusaurus-search-local"),
-      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
       ({
         // `hashed` is recommended as long-term-cache of index file is possible.
         hashed: true,
