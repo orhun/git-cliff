496d4f016635d3807459a211782e11a2222fd721
Daniel Perez <danielpza@protonmail.com>
2026-04-03 00:10:50 -0700

    update comment

--- git-cliff-core/src/lib.rs
+++ git-cliff-core/src/lib.rs
@@ -47,8 +47,8 @@ pub mod template;
 /// Default configuration file. This is used for the user stored global configuration and embedded
 /// configuration.
 pub const DEFAULT_CONFIG: &str = "cliff.toml";
-/// List of configuration files. This list of files is used for finding the configuration file
-/// relative to the project directory.
+/// List of possible configuration file location. This list of files is used for finding the configuration file
+/// relative to the project directory. The first file has the highest priority.
 pub const CONFIG_FILES: &[&str] = &["cliff.toml", ".cliff.toml", ".config/cliff.toml"];
 /// Default output file.
 pub const DEFAULT_OUTPUT: &str = "CHANGELOG.md";
