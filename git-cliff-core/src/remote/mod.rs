8d7e69e789f04295a30656add062b6ed08de8a85
Helmut K. C. Tessarek <tessarek@evermeet.cx>
2026-04-07 19:45:51 -0400

    refactor(deps): replace dirs_next with etcetera

--- git-cliff-core/src/remote/mod.rs
+++ git-cliff-core/src/remote/mod.rs
@@ -23,6 +23,7 @@ use std::fmt::Debug;
 use std::time::Duration;
 
 use dyn_clone::DynClone;
+use etcetera::{BaseStrategy, choose_base_strategy};
 use http_cache_reqwest::{CACacheManager, Cache, CacheMode, HttpCache, HttpCacheOptions};
 use reqwest::Client;
 use reqwest::header::{HeaderMap, HeaderValue};
@@ -99,6 +100,8 @@ impl Remote {
         if !self.is_set() {
             return Err(Error::RemoteNotSetError);
         }
+        let strategy = choose_base_strategy()
+            .expect("cannot determine current OS's default strategy (layout)");
         let mut headers = HeaderMap::new();
         headers.insert(
             reqwest::header::ACCEPT,
@@ -126,13 +129,7 @@ impl Remote {
             .with(Cache(HttpCache {
                 mode: CacheMode::Default,
                 manager: CACacheManager {
-                    path: dirs::cache_dir()
-                        .ok_or_else(|| {
-                            Error::DirsError(String::from(
-                                "failed to find the user's cache directory",
-                            ))
-                        })?
-                        .join(env!("CARGO_PKG_NAME")),
+                    path: strategy.cache_dir().join(env!("CARGO_PKG_NAME")),
                 },
                 options: HttpCacheOptions::default(),
             }))
