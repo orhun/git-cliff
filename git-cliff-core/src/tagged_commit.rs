4bece48de0a1d775f7afdc54e3169dfacb485669
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2026-07-06 21:02:58 +0200

    fix: retain all tagged commits

--- git-cliff-core/src/tagged_commit.rs
+++ git-cliff-core/src/tagged_commit.rs
@@ -127,15 +127,11 @@ impl<'a> TaggedCommits<'a> {
 
     /// Retains only the tags specified by the predicate.
     pub fn retain(&mut self, mut f: impl FnMut(&Tag) -> bool) {
+        self.tags.retain(|_, tag| f(tag));
         self.tag_indexes.retain(|&idx| {
-            let panic_msg = "invalid TaggedCommits state";
-            let (commit_of_tag, _) = self.commits.get_index(idx).expect(panic_msg);
-            let tag = self.tags.get(commit_of_tag).expect(panic_msg);
-            let retain = f(tag);
-            if !retain {
-                self.tags.shift_remove(commit_of_tag);
-            }
-            retain
+            self.commits
+                .get_index(idx)
+                .is_some_and(|(commit, _)| self.tags.contains_key(commit))
         });
     }
 
